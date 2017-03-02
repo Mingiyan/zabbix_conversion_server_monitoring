81efcf4c43dbca597849d01207a77cb291b1fb6c
Mingiyan D <vvvdmingiyan@gmail.com>
2017-03-02 12:55:42 +0300

    Update mfscheck.py

--- mfscheck.py
+++ mfscheck.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python
 
 import sys, os
-import urllib
-
+import urllib2
+from urllib2 import URLError
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
 link = 'http://' + sys.argv[1] + '/mfs/'
 
-f = urllib.urlopen(link)
-myfile = f.read()
-
-if 'Mirapolis File Conversion Server' in myfile:
-    print ("1")
-else:
-    sys.exit(1)
+try:
+    f = urllib2.urlopen(link, timeout=1.0)
+    myfile = f.read()
+    if 'Mirapolis File Conversion Server' in myfile:
+        print ("1")
+    else:
+        print ("0")
+except URLError as e:
+    print ("0")
